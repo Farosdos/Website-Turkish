5b08a50e67145cc85ca6ddc8e20577bbcd24c18e
Szymon Pel <szymonpel@proton.me>
2025-01-26 18:32:12 +0100

    chore: move gradient background to its separate component

--- src/app/downloads/page.tsx
+++ src/app/downloads/page.tsx
@@ -1,6 +1,7 @@
 import { Download } from 'lucide-react';
 import { z } from 'zod';
 import { Button } from '~/components/ui/button';
+import GradientBackground from '~/components/ui/gradient-background';
 import { BuildSchema, getBuilds } from '~/lib/jenkins';
 
 import { VersionBuildsTable } from './version-builds-table';
@@ -31,18 +32,7 @@ export default async function DownloadsPage() {
 
   return (
     <div className='relative isolate'>
-      <div
-        aria-hidden='true'
-        className='absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80'
-      >
-        <div
-          className='relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-fuchsia-500 to-sky-600 opacity-13 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]'
-          style={{
-            clipPath:
-              'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-          }}
-        />
-      </div>
+      <GradientBackground />
 
       <div className='mx-auto max-w-7xl px-4 py-16 sm:px-6 lg:px-8'>
         <div className='mx-auto max-w-3xl text-center'>
--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -1,5 +1,6 @@
 import { Button } from '~/components/ui/button';
 import { Card } from '~/components/ui/card';
+import GradientBackground from '~/components/ui/gradient-background';
 
 import Image from 'next/image';
 import Link from 'next/link';
@@ -43,18 +44,8 @@ export default async function TeamPage() {
 
   return (
     <div className='relative text-white' role='main' aria-label='Team page'>
-      <div
-        aria-hidden='true'
-        className='absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80'
-      >
-        <div
-          className='relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-fuchsia-500 to-sky-600 opacity-13 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]'
-          style={{
-            clipPath:
-              'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-          }}
-        />
-      </div>
+      <GradientBackground />
+
       {/* Hero Section */}
       <div className='mx-auto max-w-7xl px-4 py-16 sm:px-6 sm:py-24 lg:px-8'>
         <div className='mx-auto max-w-3xl text-center'>
