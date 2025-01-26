ea98cd3b40923ae1ed95c2b7ab40a06f2d209a84
Ventitoja <tyminski.trujka+1@gmail.com>
2025-01-26 18:45:50 +0100

    fix

--- src/app/downloads/page.tsx
+++ src/app/downloads/page.tsx
@@ -27,19 +27,7 @@ export default async function DownloadsPage() {
 
   return (
     <main className='relative isolate min-h-screen' role='main' aria-label='Downloads page'>
-      <div
-        role='presentation'
-        className='absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80'
-        aria-hidden='true'
-      >
-        <div
-          className='relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-fuchsia-500 to-sky-600 opacity-13 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]'
-          style={{
-            clipPath:
-              'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-          }}
-        />
-      </div>
+      <GradientBackground />
 
       <article className='mx-auto max-w-7xl px-4 py-16 sm:px-6 sm:py-24 lg:px-8'>
         <header className='mx-auto max-w-3xl text-center'>
@@ -49,7 +37,7 @@ export default async function DownloadsPage() {
           </p>
 
           {latestBuild?.downloadUrl && (
-            <Button size='lg' asChild className='mt-6 w-full sm:mt-8 sm:w-auto'>
+            <Button size='lg' asChild className='mt-6 sm:mt-8'>
               <a
                 href={latestBuild.downloadUrl}
                 download
