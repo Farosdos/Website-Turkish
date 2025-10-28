76dfa542e0d3f28aab2e820e0cb07aa09ff92641
Farosdos <132766064+Farosdos@users.noreply.github.com>
2025-10-28 19:09:09 +0300

    Delete Website-main directory

--- Website-main/src/config/jenkins.ts
+++
@@ -1,6 +0,0 @@
-export const jenkinsConfig = {
-  baseUrl: process.env.JENKINS_BASE_URL || 'https://jenkins.canvasmc.io',
-  job: 'Canvas',
-  treeQuery:
-    'allBuilds[number,url,displayName,result,timestamp,artifacts[relativePath],changeSet[items[msg,commitId,comment]]]',
-} as const;
--- Website-main/src/config/plausible.ts
+++
@@ -1,9 +0,0 @@
-import { siteConfig } from './site';
-
-export const plausibleConfig = {
-  selfHosted: true,
-  customDomain: process.env.PLAUSIBLE_CUSTOM_DOMAIN || 'https://plausible.meteors.cc',
-  domain: new URL(siteConfig.url).hostname,
-  trackFileDownloads: true,
-  trackOutboundLinks: true,
-} as const;
